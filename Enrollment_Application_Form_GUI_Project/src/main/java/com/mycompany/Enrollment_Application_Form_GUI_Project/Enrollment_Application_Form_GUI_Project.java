2cf207c9ece26e76e0af483cce63b2865e4828e8
joeDonuts <136735721+joeDonuts@users.noreply.github.com>
2023-08-20 23:09:55 +0800

    Add files via upload

---
+++ Enrollment_Application_Form_GUI_Project/src/main/java/com/mycompany/Enrollment_Application_Form_GUI_Project/Enrollment_Application_Form_GUI_Project.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.Enrollment_Application_Form_GUI_Project;
+
+/**
+ *
+ * @author joeze
+ */
+public class Enrollment_Application_Form_GUI_Project {
+
+    public static void main(String[] args) {
+        Application_Form AppForm = new Application_Form();
+        AppForm.setVisible(true);    }
+}
